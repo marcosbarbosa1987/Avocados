c8407745920e8caa7bbc254a03ec64c1fe6eac9c
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-02-21 19:36:07 -0300

    First commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Marcos Barbosa on 21/02/21.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
