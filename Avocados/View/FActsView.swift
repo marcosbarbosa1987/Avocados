bd2fad203d6ba42ea6411378eb327bb7846b1cae
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-02-23 21:46:08 -0300

    Create FactsView;

---
+++ Avocados/View/FActsView.swift
@@ -0,0 +1,27 @@
+//
+//  FActsView.swift
+//  Avocados
+//
+//  Created by Marcos Barbosa on 23/02/21.
+//
+
+import SwiftUI
+
+struct FActsView: View {
+    
+    // MARK: - Properties
+    
+    // MARK: - Body
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - Preview
+
+struct FActsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FActsView()
+    }
+}
