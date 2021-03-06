85d636ec43e26d3f7189234cd28d1307bc71a0c3
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-03-05 21:17:19 -0300

    Finalize Avocado app;

--- Avocados/View/SettingsView.swift
+++ Avocados/View/SettingsView.swift
@@ -11,10 +11,109 @@ struct SettingsView: View {
     
     // MARK: - Properties
     
+    @State private var enableNotification: Bool = true
+    @State private var backgroundRefresh: Bool = false
+    
     // MARK: - Body
     
     var body: some View {
-        Text("SettingsView")
+        VStack(alignment: .center, spacing: 0) {
+            
+            // Header
+            
+            VStack(alignment: .center, spacing: 5) {
+                
+                Image("avocado")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.top)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .shadow(color: Color("ColorBlackTransparentLight"), radius: 8, x: 0, y: 4)
+                
+                Text("Avocados".uppercased())
+                    .font(.system(.title, design: .serif))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("ColorGreenAdaptive"))
+            }
+            .padding()
+            
+            
+            // Form
+            
+            Form {
+                // MARK: - Section 01
+                
+                Section(header: Text("General Settings")) {
+                    Toggle(isOn: $enableNotification) {
+                       Text("Enable notification")
+                    }
+                    
+                    Toggle(isOn: $backgroundRefresh) {
+                        Text("Background refresh")
+                    }
+                }
+                
+                // MARK: - Section 02
+                
+                Section(header: Text("Application")) {
+                    
+                    if enableNotification {
+                        HStack {
+                            Text("Product")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Avocado Recipes")
+                        }
+                        
+                        HStack {
+                            Text("Compatibility")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("iPhone & iPad")
+                        }
+                        
+                        HStack {
+                            Text("Developer")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Marcos Barbosa")
+                        }
+                        
+                        HStack {
+                            Text("Designer")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Robert Petras")
+                        }
+                        
+                        HStack {
+                            Text("Website")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("https://marcosbarbosa.dev")
+                        }
+                        
+                        HStack {
+                            Text("Version")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Avocado Recipes")
+                        }
+                    } else {
+                        HStack {
+                            Text("Personal message")
+                                .foregroundColor(.gray)
+                            
+                            Spacer()
+                            Text("Happy Coding!")
+                        }
+                    }
+                    
+                    
+                }
+            }
+        }
+        .frame(maxWidth: 640)
     }
 }
 
